4d38f07fe015124b0d7465e75c48b8155d342d53
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2022-11-11 01:22:54 +0100

    lets upgrade js bootcamp

---
+++ DOM.js
@@ -0,0 +1,8 @@
+//Write a JavaScript program to set the background color of a paragraph
+
+const backgroundChange = document.getElementById("paragraph")
+backgroundChange.style.backgroundColor = "orange";
+backgroundChange.style.fontSize = "24px";
+backgroundChange.style.padding = "30px";
+backgroundChange.style.margin = "30px";
+backgroundChange.style.borderRadius = "20px";
---
+++ loops_and_functions.js
@@ -0,0 +1,23 @@
+//Write a statement to find the largest of five numbers. Display an alert box to show the result
+
+a = 9;
+b = 54;
+c = 86;
+d = 42
+e = 35;
+
+if (a > b && a > c && a > d && a > e){
+    alert("The largest of the five numbers is 9");
+}
+else if (b > a && b > c && b > d && b > e){
+    alert("The largest of the five numbers is 54");
+}
+else if (c > a && c > b && c > d && c > e){
+    alert("The largest of the five numbers is 86");
+}
+else if (d > a && d > b && d > c && d > e){
+    alert("The largest of the five numbers is 42");
+}
+else{
+    alert("The largest of the five numbers is 35");
+}
